979304bd90f8671de4b6c5b65975fca7093bb0e4
llDeRuNll <eduardprudiys@gmail.com>
2025-07-13 17:42:05 -0400

    middlewars, post, patch, delete, controllers

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,10 +1,19 @@
 import express from 'express';
-import { getAllContacts, getContact } from '../controllers/contacts.js';
+import {
+  getAllContacts,
+  getContact,
+  createContact,
+  patchContact,
+  removeContact,
+} from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const router = express.Router();
 
 router.get('/', ctrlWrapper(getAllContacts));
 router.get('/:contactId', ctrlWrapper(getContact));
+router.post('/', ctrlWrapper(createContact));
+router.patch('/:contactId', ctrlWrapper(patchContact));
+router.delete('/:contactId', ctrlWrapper(removeContact));
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -1,60 +1,24 @@
 import express from 'express';
 import cors from 'cors';
-
-import { getContacts, getContactById } from './services/contacts.js';
+import contactsRouter from './routers/contacts.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 export const setupServer = () => {
   const app = express();
   app.use(cors());
   app.use(express.json());
 
-  app.get('/contacts', async (req, res) => {
-    const contactsData = await getContacts();
-
-    res.json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      contactsData,
-    });
-  });
-
   app.get('/', (req, res) => {
     res.send('<h1>Contacts main page</h1>');
   });
 
-  app.get('/contacts/:contactId', async (req, res) => {
-    const { contactId } = req.params;
+  app.use('/contacts', contactsRouter);
 
-    const contactData = await getContactById(contactId);
+  app.use(notFoundHandler);
 
-    if (!contactData) {
-      return res.status(404).json({
-        message: 'Contact not found',
-      });
-    }
-
-    res.json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
-      contactData,
-    });
-  });
-
-  app.use((error, req, res, next) => {
-    const { status, message } = error;
-    res.status(status).json({
-      status,
-      message,
-    });
-  });
-
-  app.get((req, res) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
+  app.use(errorHandler);
 
   const port = Number(process.env.PORT) || 3000;
-
   app.listen(port, () => console.log(`Server is running on ${port} port`));
 };
